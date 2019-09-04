c94fb6a32d0b2097f363e42eb8e1e498f6805553
Suparada Saitalae <ann.suparada@gmail.com>
2019-09-04 12:57:59 -0500

    router

--- api/server.js
+++ api/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const server = express();
+const productsRouter = require('../product/productsRouter')
+const suppliersRouter = require('../suppliers/suppliersRouter');
+
+server.use(express.json())
+server.use('/products', productsRouter);
+server.use('/suppliers', suppliersRouter);
 
 server.get('/', (req, res) => {
     res.status(200).send('API running')
+})
+
+server.get('/clients', (req, res) => {
+   
 })
 
 module.exports = server;
